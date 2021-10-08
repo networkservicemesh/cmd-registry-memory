435e224c93398c01263a34f967fecbb7439d4d09
Mikhail Avramenko <avramenkomihail15@gmail.com>
2021-10-08 11:19:15 +0700

    Added config option for log level
    Signed-off-by: Mikhail Avramenko <avramenkomihail15@gmail.com>

--- main.go
+++ main.go
@@ -51,6 +51,7 @@ type Config struct {
 	ListenOn         []url.URL     `default:"unix:///listen.on.socket" desc:"url to listen on." split_words:"true"`
 	ProxyRegistryURL url.URL       `desc:"url to the proxy registry that handles this domain" split_words:"true"`
 	ExpirePeriod     time.Duration `default:"1s" desc:"period to check expired NSEs" split_words:"true"`
+	LogLevel         string        `default:"INFO" desc:"Log level" split_words:"true"`
 }
 
 func main() {
@@ -90,6 +91,7 @@ func main() {
 	if err := envconfig.Process("registry_memory", config); err != nil {
 		logrus.Fatalf("error processing config from env: %+v", err)
 	}
+	setLogLevel(config.LogLevel)
 
 	log.FromContext(ctx).Infof("Config: %#v", config)
 
@@ -145,4 +147,12 @@ func exitOnErr(ctx context.Context, cancel context.CancelFunc, errCh <-chan erro
 		log.FromContext(ctx).Error(err)
 		cancel()
 	}(ctx, errCh)
+}
+
+func setLogLevel(level string) {
+	l, err := logrus.ParseLevel(level)
+	if err != nil {
+		logrus.Fatalf("invalid log level %s", level)
+	}
+	logrus.SetLevel(l)
 }
